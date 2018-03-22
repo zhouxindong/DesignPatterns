d56120b8805dcd3efcbc1eed1a62ab01fb05da9f
zhouxindong <zhouxindong@163.com>
2018-03-22 15:17:25 +0800

    Adapter Finished

---
+++ Adapter/Turkey.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Adapter
+{
+    public interface ITurkey
+    {
+        void Gobble();
+        void Fly();
+    }
+
+    public class WildTurkey : ITurkey
+    {
+        public void Gobble()
+        {
+            Console.WriteLine("Gobble");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Flying a short distance");
+        }
+    }
+
+}
