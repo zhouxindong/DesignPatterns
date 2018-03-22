49c1ae9dfac2cfba98d82e0524bcc5ec65541334
zhouxindong <zhouxindong@163.com>
2018-03-22 16:38:01 +0800

    Command Finished

---
+++ Command/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commands = new List<ICommand>();
+            commands.Add(new NullCommand());
+            commands.Add(new WriteCommand());
+            var com = new MacroCommand(commands);
+            com.Execute();
+
+            Console.ReadKey();
+        }
+    }
+}
