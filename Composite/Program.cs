54ce5f9bc49f3aa495a279bbad6f050e18ee51fb
zhouxindong <zhouxindong@163.com>
2018-03-23 10:30:43 +0800

    Composite Finished

---
+++ Composite/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IShape circle1 = new Circle();
+            IShape triangle1 = new Triangle();
+            var composite = new CompositeShape();
+            composite.Add(circle1);
+            composite.Add(triangle1);
+
+            List<IShape> shapes = new List<IShape> {circle1, triangle1, composite};
+
+            shapes.ForEach(shape => shape.Draw());
+            Console.ReadKey();
+
+            var menu_hot = new Menu("Hot", "hot meal");
+            var menu_meat = new Menu("Meat", "meat");
+            var menu_beef = new MenuItem("Beef", "beef", false, 32.89);
+            var menu_proch = new MenuItem("Porch", "porch", false, 20.32);
+            menu_meat.Add(menu_beef);
+            menu_meat.Add(menu_proch);
+            menu_hot.Add(menu_meat);
+
+            var menu_veg = new Menu("Veg", "veg");
+            var menu_omio = new MenuItem("Omio", "omio", true, 3.84);
+            var menu_tomato = new MenuItem("Tomato", "tomato", true, 5.33);
+            var menu_potato = new MenuItem("Potato", "potato", true, 2.12);
+            menu_veg.Add(menu_omio);
+            menu_veg.Add(menu_tomato);
+            menu_veg.Add(menu_potato);
+            menu_hot.Add(menu_veg);
+
+            menu_hot.Print();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Composite/Shape.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Composite
+{
+    /// <summary>
+    /// ensure consistency of use single object and composite object
+    /// </summary>
+    public interface IShape
+    {
+        void Draw();
+    }
+
+    public class Circle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Draw a Circle");
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Draw a Triangle");
+        }
+    }
+
+    public class CompositeShape : IShape
+    {
+        private readonly IList<IShape> _shapes = new List<IShape>();
+
+        public void Add(IShape shape)
+        {
+            _shapes.Add(shape);
+        }
+
+        public void Draw()
+        {
+            foreach (var shape in _shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+}
