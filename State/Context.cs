933c2e1eca34282c7b6fdb9c0fdaec44e4dd5a92
zhouxindong <zhouxindong@163.com>
2018-03-26 15:22:44 +0800

    State Finished

---
+++ State/Context.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Mime;
+
+namespace State
+{
+    public class Context
+    {
+        public IState State { get; set; }
+
+        public Context(IState state)
+        {
+            State = state;
+        }
+
+        public void Request()
+        {
+            State.Handle();
+        }
+    }
+}
---
+++ State/IState.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace State
+{
+    public interface IState
+    {
+        void Handle();
+    }
+
+    public class GoodState : IState
+    {
+        public void Handle()
+        {
+            Console.WriteLine("I'm good!");
+        }
+    }
+
+    public class NormalState : IState
+    {
+        public void Handle()
+        {
+            Console.WriteLine("I'm normal!");
+        }
+    }
+
+    public class BadlyState : IState
+    {
+        public void Handle()
+        {
+            Console.WriteLine("I'm badly!");
+        }
+    }
+}
